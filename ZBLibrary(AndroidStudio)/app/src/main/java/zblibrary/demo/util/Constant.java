d401c1385fb71d520b3feab16c5173858324dc79
LemonTommy <1184482681@qq.com>
2016-10-18 17:40:13 +0800

    同步eclipse版至studio版

--- ZBLibrary(AndroidStudio)/app/src/main/java/zblibrary/demo/util/Constant.java
+++ ZBLibrary(AndroidStudio)/app/src/main/java/zblibrary/demo/util/Constant.java
@@ -25,13 +25,8 @@ public class Constant {
 	public static final String APP_OFFICIAL_WEIBO = "weibo.com/5225556360";
 	public static final String APP_OFFICIAL_BLOG = "http://my.oschina.net/u/2437072";
 	public static final String APP_OFFICIAL_EMAIL = "1184482681@qq.com";
-	public static final String APP_DOWNLOAD_WEBSITE = "https://github.com/TommyLemon/ZBLibrary";
+	public static final String APP_DOWNLOAD_WEBSITE = "http://files.cnblogs.com/files/tommylemon/ZBLibraryDemoApp.apk";
 
 	public static final String APP_DEVELOPER_WEBSITE = "https://github.com/TommyLemon";
-	
-	/**
-	 * 这是我们开发的游戏[蝙蝠饿了]的下载链接，请多多支持，欢迎吐槽，欢迎反馈~
-	 */
-	public static final String HUNGRY_BAT_DOWNLOAD_WEBSITE = "http://www.wandoujia.com/apps/com.pianfu.hungrybat/download";
 
 }
