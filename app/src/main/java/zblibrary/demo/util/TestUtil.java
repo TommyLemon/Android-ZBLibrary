4466a0bf650fc9e8c536263dbd5e7149697ba402
TommyLemon <1184482681@qq.com>
2019-11-01 21:09:21 +0800

    替换加载失败的图片

--- app/src/main/java/zblibrary/demo/util/TestUtil.java
+++ app/src/main/java/zblibrary/demo/util/TestUtil.java
@@ -71,25 +71,25 @@ public class TestUtil {
 	 * 图片地址，仅供测试用
 	 */
 	public static String[] URLS = {
-		"http://static.oschina.net/uploads/user/1218/2437072_100.jpg?t=1461076033000",
-		"http://common.cnblogs.com/images/icon_weibo_24.png",
-		"http://static.oschina.net/uploads/user/585/1170143_50.jpg?t=1390226446000",
-		"http://static.oschina.net/uploads/user/1174/2348263_50.png?t=1439773471000",
-		"http://common.cnblogs.com/images/wechat.png",
-		"https://static.oschina.net/uploads/user/1344/2689427_200.jpg?t=1484029839000",
-		"http://static.oschina.net/uploads/user/998/1997902_50.jpg?t=1407806577000",
-		"http://static.oschina.net/uploads/user/1/3064_50.jpg?t=1449566001000",
-		"http://static.oschina.net/uploads/user/51/102723_50.jpg?t=1449212504000",
-		"http://static.oschina.net/uploads/user/48/96331_50.jpg",
-		"http://static.oschina.net/uploads/user/48/97721_50.jpg?t=1451544779000",
-		"http://static.oschina.net/uploads/user/48/96289_50.jpg?t=1452751699000",
-		"http://static.oschina.net/uploads/user/19/39085_50.jpg",
-		"http://static.oschina.net/uploads/user/1332/2664107_50.jpg?t=1457405500000",
-		"http://static.oschina.net/uploads/user/1385/2770216_50.jpg?t=1464405516000",
-		"http://static.oschina.net/uploads/user/427/855532_50.jpg?t=1435030876000",
-		"http://static.oschina.net/uploads/user/629/1258821_50.jpg?t=1378063141000",
-		"http://static.oschina.net/uploads/user/1200/2400261_50.png?t=1439638750000",
-		"https://static.oschina.net/uploads/user/1143/2286031_50.jpg?t=1468900004000"
+			"http://static.oschina.net/uploads/user/1218/2437072_100.jpg?t=1461076033000",
+			"http://common.cnblogs.com/images/icon_weibo_24.png",
+			"http://static.oschina.net/uploads/user/585/1170143_50.jpg?t=1390226446000",
+			"http://static.oschina.net/uploads/user/1174/2348263_50.png?t=1439773471000",
+			"http://common.cnblogs.com/images/wechat.png",
+			"http://static.oschina.net/uploads/user/998/1997902_50.jpg?t=1407806577000",
+			"http://static.oschina.net/uploads/user/1/3064_50.jpg?t=1449566001000",
+			"http://static.oschina.net/uploads/user/51/102723_50.jpg?t=1449212504000",
+			"http://static.oschina.net/uploads/user/48/96331_50.jpg",
+			"http://static.oschina.net/uploads/user/48/97721_50.jpg?t=1451544779000",
+			"http://static.oschina.net/uploads/user/48/96289_50.jpg?t=1452751699000",
+			"http://static.oschina.net/uploads/user/19/39085_50.jpg",
+			"http://static.oschina.net/uploads/user/1332/2664107_50.jpg?t=1457405500000",
+			"http://static.oschina.net/uploads/user/1385/2770216_50.jpg?t=1464405516000",
+			"http://static.oschina.net/uploads/user/427/855532_50.jpg?t=1435030876000",
+			"http://static.oschina.net/uploads/user/629/1258821_50.jpg?t=1378063141000",
+			"http://static.oschina.net/uploads/user/1200/2400261_50.png?t=1439638750000",
+			"http://pic.cnblogs.com/face/u373473.jpg?id=07231933",
+			"http://pic.cnblogs.com/face/221462/20131226172100.png"
 	};
 
 	public static String getPicture(int index) {
